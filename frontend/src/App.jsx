54321c07a51feeea9baf26c37e6d6dc2ba1b0ab4
Gajendra Singh <gajendrasinghrajput917@gmail.com>
2026-06-24 20:40:38 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+    </>
+  )
+}
+
+export default App
