ddff752fd27b7119d6cbf4369a488d9ce5aa9d12
Gajendra Singh <gajendrasinghrajput917@gmail.com>
2026-07-14 01:30:13 +0530

    database set-up

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
+const mongo_URI = process.env.mongoDBURI;
+
+// connect to database
+try {
+  mongoose.connect(mongo_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('Connected to MongoDB');
+} catch (error) {
+  console.error('Error connecting to MongoDB:', error);
+}
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Book Store!');
 });
 
 app.listen(PORT, () => {
